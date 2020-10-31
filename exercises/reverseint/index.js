8bdbda3f8cd0677d24369e12defafeddf42c642d
sfespinosa <scottjames.espinosa@gmail.com>
2020-10-31 12:01:45 -0700

    reverse int

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,13 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+    if (n === 0) return 0
+    numStr = Math.abs(n).toString()
+    numStr = parseInt(numStr.split('').reverse().join(''))
+    return Math.sign(n) === 1 ? numStr : (numStr * -1)
+}
+
+reverseInt(-5)
 
 module.exports = reverseInt;
