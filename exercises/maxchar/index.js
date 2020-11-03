e42a1062fe31639384133f54beb621a7ed962ddf
sfespinosa <scottjames.espinosa@gmail.com>
2020-11-02 18:28:23 -0800

    maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,27 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    const chars = new Map()
+    for (let char of str){
+        if (chars.has(char)){
+            chars.set(char, chars.get(char) + 1)
+        } else {
+            chars.set(char, 1)
+        }
+    }
+    
+    let max = 0
+    let maxKey
+    chars.forEach((value, key) => {
+        if (value > max) {
+            max = value
+            maxKey = key
+        }
+    })
+    
+    return maxKey
+}
 
+maxChar('abcc')
 module.exports = maxChar;
