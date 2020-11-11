c8c0f4c2b290db4da050cb37b0fa016c9a4d2891
sfespinosa <scottjames.espinosa@gmail.com>
2020-11-10 21:40:39 -0800

    anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,50 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+    // my solution - building character map
+    // stringA = stringA.replace(/[^\w]/g, "").toLowerCase()
+    // stringB = stringB.replace(/[^\w]/g, "").toLowerCase()
+    // aMap = {}
+    // bMap = {}
+    // if (stringA.length !== stringB.length) {
+    //     return false
+    // } else {
+    //     for (let char of stringA) {
+    //         if (aMap[char]) {
+    //             aMap[char] = aMap[char] + 1
+    //         } else {
+    //             aMap[char] = 1
+    //         }
+    //     }
+
+    //     for (let char of stringB) {
+    //         if (bMap[char]) {
+    //             bMap[char] = bMap[char] + 1
+    //         } else {
+    //             bMap[char] = 1
+    //         }
+    //     }
+    // }
+
+    // if (Object.keys(aMap).length !== Object.keys(bMap).length) {
+    //     return false
+    // } else {
+    //     for (let key in aMap) {
+    //         if (aMap[key] !== bMap[key]) {
+    //             return false
+    //         }
+    //     }
+    // }
+
+    // return true
+
+    // separating into array and sorting
+    const clearStr = str => {
+        return str.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('')
+    }
+
+    return clearStr(stringA) === clearStr(stringB)
+}
 
 module.exports = anagrams;
