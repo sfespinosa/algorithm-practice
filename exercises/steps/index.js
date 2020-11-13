6e4478e3b50d88d3337da93f6be8942bfc90db23
sfespinosa <scottjames.espinosa@gmail.com>
2020-11-13 15:41:52 -0800

    steps with recursion

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,21 +17,39 @@
 //       '### '
 //       '####'
 
-function steps(n) {
-    // my solution
-    let count = 1
-    while (count <= n) {
-        let log = ''
-        for (let i = 1; i < count + 1; i++) {
-            log = log + '#'
-        }
-        for (let j = 1; j < n - count + 1; j++) {
-            log = log + ' '
-        }
+// my solution
+// function steps(n) {
+//     let count = 1
+//     while (count <= n) {
+//         let log = ''
+//         for (let i = 1; i < count + 1; i++) {
+//             log = log + '#'
+//         }
+//         for (let j = 1; j < n - count + 1; j++) {
+//             log = log + ' '
+//         }
 
-        console.log(log)
-        count++
+//         console.log(log)
+//         count++
+//     }
+// }
+
+// with recursion
+function steps(n, row = 0, stair = '') {
+    if (n === row) {
+        return
     }
-}
 
+    if (n === stair.length) {
+        console.log(stair)
+        stair = ''
+        row++
+    } else if (stair.length <= row) {
+        stair += '#'
+    } else {
+        stair += ' '
+    }
+
+    steps(n, row, stair)
+}
 module.exports = steps;
