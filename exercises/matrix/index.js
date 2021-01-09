e34c07908cb2ba73384d29d41904b360dad5d15f
sfespinosa <scottjames.espinosa@gmail.com>
2021-01-09 12:03:04 -0800

    thru matrix

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,48 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+  const results = [];
+  for (let i = 0; i < n; i++) {
+    results.push([]);
+  };
+
+  let startRow = 0;
+  let startColumn = 0;
+  let endRow = n - 1;
+  let endColumn = n - 1;
+  let counter = 1;
+  while (startRow <= endRow && startColumn <= endColumn) {
+    // top row
+    for (let i = startColumn; i <= endColumn; i++) {
+      results[startRow][i] = counter;
+      counter++
+    }
+    startRow++
+
+    // end column
+    for (let i = startRow; i <= endRow; i++) {
+      results[i][endColumn] = counter;
+      counter++;
+    }
+    endColumn--;
+
+    // end row
+    for (let i = endColumn; i >= startColumn; i--) {
+      results[endRow][i] = counter;
+      counter++;
+    }
+    endRow--;
+
+    // start column
+    for (let i = endRow; i >= startRow; i--) {
+      results[i][startColumn] = counter;
+      counter++;
+    }
+    startColumn++;
+  }
+
+  return results
+}
 
 module.exports = matrix;
