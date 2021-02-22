dd019b77d399a26279d8250acf4240daae77f4c3
sfespinosa <scottjames.espinosa@gmail.com>
2021-02-22 15:14:29 -0800

    completed trees

--- exercises/tree/index.js
+++ exercises/tree/index.js
@@ -10,8 +10,43 @@
 // on the tree class.  Each method should accept a
 // function that gets called with each element in the tree
 
-class Node {}
+class Node {
+  constructor(data){
+    this.data = data;
+    this.children = [];
+  }
 
-class Tree {}
+  add(data){
+    this.children = [...this.children, new Node(data)]
+  }
+
+  remove(data){
+    this.children = this.children.filter((node) => node.data !== data)
+  }
+}
+
+class Tree {
+  constructor(){
+    this.root = null;
+  }
+
+  traverseBF(fn){
+    const children = [this.root]
+    while (children.length) {
+      let node = children.shift()
+      fn(node)
+      children.push(...node.children)
+    }
+  }
+
+  traverseDF(fn){
+    const children = [this.root]
+    while (children.length) {
+      let node = children.shift();
+      children.unshift(...node.children)
+      fn(node)
+    }
+  }
+}
 
 module.exports = { Tree, Node };
