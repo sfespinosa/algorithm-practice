7e20ca9959f06576e7632e9289eebd73e3fbaa82
sfespinosa <scottjames.espinosa@gmail.com>
2021-02-22 22:10:55 -0800

    completed sorting

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -2,19 +2,54 @@
 // Implement bubbleSort, selectionSort, and mergeSort
 
 function bubbleSort(arr) {
-
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j+1]
+        arr[j+1] = arr[j]
+        arr[j] = temp
+      }
+    }
+  }
+  return arr
 }
 
 function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let minIndex = i
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j
+      }
+    }
 
+    let temp = arr[i]
+    arr[i] = arr[minIndex]
+    arr[minIndex] = temp
+  }
+
+  return arr
 }
 
 function mergeSort(arr) {
+  if (arr.length < 2) return arr
 
+  const midpoint = Math.floor(arr.length / 2)
+  const left = arr.slice(0, midpoint)
+  const right = arr.slice(midpoint)
+  return merge(mergeSort(left), mergeSort(right))
 }
 
 function merge(left, right) {
-
+  let results = []
+  while (left.length && right.length){
+    if (left[0] < right[0]) {
+      results.push(left.shift())
+    } else {
+      results.push(right.shift())
+    }
+  }
+  return results.concat(left).concat(right)
 }
 
 module.exports = { bubbleSort, selectionSort, mergeSort, merge };
