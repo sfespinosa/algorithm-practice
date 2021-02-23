d13bd64f846f252e209b4de138f62a26a5e0674c
sfespinosa <scottjames.espinosa@gmail.com>
2021-02-22 21:38:01 -0800

    completed events

--- exercises/events/index.js
+++ exercises/events/index.js
@@ -4,16 +4,30 @@
 // have methods 'on', 'trigger', and 'off'.
 
 class Events {
+  constructor(){
+    this.events = new Map();
+  }
   // Register an event handler
-  on(eventName, callback) {}
+  on(eventName, callback) {
+    if (!this.events.has(eventName)) {
+      this.events.set(eventName, [])
+    }
+
+    this.events.set(eventName, [...this.events.get(eventName), callback])
+  }
 
   // Trigger all callbacks associated
   // with a given eventName
-  trigger(eventName) {}
+  trigger(eventName) {
+    const callbacks = this.events.get(eventName)
+    if (callbacks) callbacks.forEach((callback) => callback())
+  }
 
   // Remove all event handlers associated
   // with the given eventName
-  off(eventName) {}
+  off(eventName) {
+    this.events.delete(eventName)
+  }
 }
 
 module.exports = Events;
