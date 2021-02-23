b6aa6a5cf77c90bc0de82ab43f2ac64dd4c5dba9
sfespinosa <scottjames.espinosa@gmail.com>
2021-02-22 17:59:16 -0800

    completed level width

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,36 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+  // my results
+  // const results = [1]
+  // let currentLevel = [root]
+  // let nextLevel = []
+  // while (currentLevel.length){
+  //   const node = currentLevel.shift()
+  //   if (node.children.length) nextLevel.push(...node.children)
+
+  //   if (!currentLevel.length && nextLevel.length) {
+  //     results.push(nextLevel.length)
+  //     currentLevel = [...nextLevel]
+  //     nextLevel = []
+  //   }
+  // }
+  // return results
+
+  const counter = [0]
+  const arr = [root, 'stop']
+  while (arr.length > 1) {
+    const current = arr.shift();
+    if (current === 'stop') {
+      counter.push(0)
+      arr.push(current)
+    } else {
+      counter[counter.length - 1]++
+      arr.push(...current.children)
+    }
+  }
+  return counter
+}
 
 module.exports = levelWidth;
